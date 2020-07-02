f1dde5ceef05e5faf7d684fafd8eb3c97d0e84c4
xmba15 <thba1590@gmail.com>
2020-07-02 23:14:29 +0900

    support None dynamic batch size

--- export_onnx.py
+++ export_onnx.py
@@ -21,7 +21,11 @@ def main(args):
     model.load_state_dict(torch.load(args.snapshot)["state_dict"])
     model.eval()
 
-    batch_size = 1
+    if args.batch_size:
+        batch_size = args.batch_size
+    else:
+        batch_size = 1
+
     x = torch.randn(batch_size, 3, dataset_params["img_h"], dataset_params["img_w"])
     torch.onnx.export(
         model,
@@ -35,6 +39,15 @@ def main(args):
         opset_version=11,
     )
 
+    if args.batch_size:
+        return
+
+    import onnx
+
+    mp = onnx.load("output.onnx")
+    mp.graph.input[0].type.tensor_type.shape.dim[0].dim_param = "None"
+    onnx.save(mp, "output.onnx")
+
 
 if __name__ == "__main__":
     import argparse
@@ -45,6 +58,7 @@ if __name__ == "__main__":
         "--dataset", type=str, required=True, help="name of the dataset to use",
     )
     parser.add_argument("--onnx_weight_file", type=str, default="output.onnx")
+    parser.add_argument("--batch_size", type=int)
     parsed_args = parser.parse_args()
 
     main(parsed_args)
